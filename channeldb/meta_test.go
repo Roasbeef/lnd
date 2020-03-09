cb1bd8af7f255c414f427b049ff540f153198306
Andras Banki-Horvath <bhandras@gmail.com>
2020-03-09 19:27:50 +0100

    kvdb+etcd: enable channeldb testing with both bdb/etcd

--- channeldb/meta_test.go
+++ channeldb/meta_test.go
@@ -3,6 +3,7 @@ package channeldb
 import (
 	"bytes"
 	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/go-errors/errors"
@@ -414,12 +415,21 @@ func TestMigrationReversion(t *testing.T) {
 	t.Parallel()
 
 	tempDirName, err := ioutil.TempDir("", "channeldb")
+	defer func() {
+		os.RemoveAll(tempDirName)
+	}()
 	if err != nil {
 		t.Fatalf("unable to create temp dir: %v", err)
 	}
 
-	cdb, err := Open(tempDirName)
+	backend, cleanup, err := kvdb.GetTestBackend(tempDirName, "cdb")
 	if err != nil {
+		t.Fatalf("unable to get test db backend: %v", err)
+	}
+
+	cdb, err := CreateWithBackend(backend)
+	if err != nil {
+		cleanup()
 		t.Fatalf("unable to open channeldb: %v", err)
 	}
 
@@ -435,12 +445,19 @@ func TestMigrationReversion(t *testing.T) {
 
 	// Close the database. Even if we succeeded, our next step is to reopen.
 	cdb.Close()
+	cleanup()
 
 	if err != nil {
 		t.Fatalf("unable to increase db version: %v", err)
 	}
 
-	_, err = Open(tempDirName)
+	backend, cleanup, err = kvdb.GetTestBackend(tempDirName, "cdb")
+	if err != nil {
+		t.Fatalf("unable to get test db backend: %v", err)
+	}
+	defer cleanup()
+
+	_, err = CreateWithBackend(backend)
 	if err != ErrDBReversion {
 		t.Fatalf("unexpected error when opening channeldb, "+
 			"want: %v, got: %v", ErrDBReversion, err)
